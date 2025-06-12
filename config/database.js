685c89f4f19eb9e5b2cf69462afadec3bc647726
ZecaMonky <zacamonky@gmail.com>
2025-06-12 14:27:25 +0500

    1.1

--- config/database.js
+++ config/database.js
@@ -11,5 +11,16 @@ module.exports = {
             }
         },
         logging: false
+    },
+    production: {
+        url: process.env.DATABASE_URL,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        logging: false
     }
 }; 
